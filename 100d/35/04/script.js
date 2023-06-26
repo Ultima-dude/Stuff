4632f54adb4958de75b77c325b9099ee46c54098
OldZaZa <zedsdmail@gmail.com>
2023-06-27 00:13:10 +0300

    input

--- 100d/35/04/script.js
+++ 100d/35/04/script.js
@@ -1,10 +1,26 @@
 "use strict"
 
 const deposit = document.forms.calculator.elements.money;
-//console.log(deposit);
+const months = document.forms.calculator.elements.months;
+const percent = document.forms.calculator.elements.interest;
+const moneyBefore = document.getElementById("money-before");
+const moneyAfter = document.getElementById("money-after");
+const divAfter = document.getElementById("height-after");
 
-deposit.addEventListener('change', calcFinalSum);
+calcDivHeight();
+calcFinalSum();
 
-function calcFinalSum(event) {
-    @
+deposit.addEventListener('input', calcFinalSum);
+months.addEventListener('change', calcFinalSum);
+percent.addEventListener('input', calcFinalSum);
+
+function calcFinalSum() {
+    calcDivHeight();
+
+    moneyBefore.textContent = deposit.value;
+    moneyAfter.textContent = Math.round(deposit.value * (1 + percent.value / 100) ** (months.value / 12))
+}
+
+function calcDivHeight() {
+    divAfter.style.height = `${100 + Number(percent.value)}px`;
 }
