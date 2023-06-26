e80605e2bde494a28e25d741b066994317ccba09
OldZaZa <zedsdmail@gmail.com>
2023-06-26 21:04:06 +0300

    change/input/cut/copy

---
+++ 100d/35/04/script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+const deposit = document.forms.calculator.elements.money;
+//console.log(deposit);
+
+deposit.addEventListener('change', calcFinalSum);
+
+function calcFinalSum(event) {
+    @
+}
