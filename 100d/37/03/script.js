a95d05e7d3f2341f69808ff06ba0c5858ca1a51e
OldZaZa <zedsdmail@gmail.com>
2023-06-28 22:44:08 +0300

    js

---
+++ 100d/37/03/script.js
@@ -0,0 +1,9 @@
+function arrayDiff(a, b) {
+    for(let i = 0; i < b.length; i++) {
+        for(let j = 0; j < a.length; j++) {
+            if(a[j] == b[i]) a.splice(j--, 1);
+        }
+    }
+
+    return a;
+}
